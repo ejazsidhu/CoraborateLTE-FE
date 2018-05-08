cef0bae6ee7298b015f2501e203d93d8ea3c5cf1
ejazsidhu@gmail.com <ejazsidhu@gmail.com>
2018-05-08 18:40:14 +0500

    dashoboard nad dashboard body pages added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-  public  logo_lg= require('../assets/images/CoraborateSmallIcon.png');
-  public  logo_mini= require('../assets/images/CoraborateSmallIcon.png');
-  public user_image=require("../assets/images/user2-160x160.jpg");
+
 }
