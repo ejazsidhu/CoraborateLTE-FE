ef0bceba61b4a41836b2551dafb87759fd8b47ac
ejazsidhu@gmail.com <ejazsidhu@gmail.com>
2018-05-24 15:03:37 +0500

    routeing is set and profile page added

--- src/app/_pages/admin-dashboard/admin-navbar/admin-navbar.component.ts
+++ src/app/_pages/admin-dashboard/admin-navbar/admin-navbar.component.ts
@@ -10,7 +10,7 @@ export class AdminNavbarComponent implements OnInit {
   public  logo_lg= require('../../../../assets/images/sk2logo.svg');
   public  codegen= require('../../../../assets/images/code-gen.png');
   public  school= require('../../../../assets/images/create-school.png');
-  public  logo_mini= require('../../../../assets/images/withoutTexLogo.png');
+  public  logo_mini= require('../../../../assets/images/withoutTextLogo.png');
   public user_image=require("../../../../assets/images/user8-128x128.jpg");
   constructor() { }
 
