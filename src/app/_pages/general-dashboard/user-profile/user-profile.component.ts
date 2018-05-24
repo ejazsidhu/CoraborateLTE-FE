0d8ccf489029c70864057aee4ff7f10b84cb7a5c
Ibrahimkhattana123 <ibrahim.khattana@gmail.com>
2018-05-24 16:28:19 +0500

    Implemented UserProfile

--- src/app/_pages/general-dashboard/user-profile/user-profile.component.ts
+++ src/app/_pages/general-dashboard/user-profile/user-profile.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { userprofilecomponentservice } from './user-profile.service'
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+  CurrentUserObj: any;
+  OwnedjoinedClasses: any;
+  constructor(private us: userprofilecomponentservice) { }
 
   ngOnInit() {
+    this.CurrentUserObj = JSON.parse(sessionStorage.getItem('CurrentUser'));
+    // console.log(this.CurrentUserObj);
+    this.GetAllClasses(this.CurrentUserObj);
+
+  }
+  GetAllClasses(user) {
+
+    this.us.GetAllClassesService(user.UserId).subscribe(data => {
+    
+      this.OwnedjoinedClasses = data.ResponseData;
+      debugger;
+      console.log("user");
+      console.log(this.OwnedjoinedClasses);
+    },
+      Error => {
+        console.log(Error);
+      });
   }
 
 }
